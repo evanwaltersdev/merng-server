6375f9baa2787a5ae59478840c625afe915e9e6d
Evan Walters <evan@evanw.uk>
2020-12-01 17:10:35 +0000

    some work on registration

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const { SECRET_KEY } = require('../../config')
+const User = require('../../models/User')
+
+module.exports = {
+    Mutuation: {
+        async register(_, { registerInput: { username, email, password, confirmPassword } }, context, info){
+            // TODO: Validare user data
+            // TODO: Make sure user doesnt already exist
+            // TODO: hash pass and create auth token
+            password = await bcrypt.hash(password, 12)
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            })
+
+            const res = await newUser.save()
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username,
+            }, SECRET_KEY, { expiresIn: '1h' })
+
+            return {
+                ...res._doc,
+                id: res.id_id,
+                token
+            }
+        }
+    }
+}
